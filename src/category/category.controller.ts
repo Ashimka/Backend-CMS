3d5aa13ea991f2cc024d3abcc35d16b4e5e79a49
Ashimka <ashimka2010@yandex.ru>
2024-08-13 23:58:53 +0500

    add: category service & category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
-import { CategoryService } from './category.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	UsePipes,
+	ValidationPipe,
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CategoryService } from './category.service'
+import { CategoryDto } from './dto/category.dto'
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+	constructor(private readonly categoryService: CategoryService) {}
+
+	@Get('by-id/:id')
+	async getById(@Param('id') id: string) {
+		return this.categoryService.getById(id)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Post('/')
+	async create(@Body() dto: CategoryDto) {
+		return this.categoryService.create(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Put(':id')
+	async update(@Param('id') id: string, @Body() dto: CategoryDto) {
+		return this.categoryService.update(id, dto)
+	}
+
+	@HttpCode(200)
+	@Auth()
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return this.categoryService.delete(id)
+	}
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { CategoryDto } from './dto/category.dto'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(private prisma: PrismaService) {}
+
+	async getById(id: string) {
+		const category = await this.prisma.category.findUnique({
+			where: {
+				id,
+			},
+		})
+
+		if (!category) throw new NotFoundException('Категория не найдена')
+
+		return category
+	}
+
+	async create(dto: CategoryDto) {
+		return this.prisma.category.create({
+			data: {
+				title: dto.title,
+				description: dto.description,
+			},
+		})
+	}
+
+	async update(id: string, dto: CategoryDto) {
+		await this.getById(id)
+
+		return this.prisma.category.update({
+			where: { id },
+			data: dto,
+		})
+	}
+
+	async delete(id: string) {
+		await this.getById(id)
+
+		return this.prisma.category.delete({
+			where: { id },
+		})
+	}
+}
