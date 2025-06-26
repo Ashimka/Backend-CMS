bde75fe41a45d091f2e66f040d5ad0c25f6a5c68
Ashimka <ashimka2010@yandex.ru>
2025-06-26 21:20:57 +0500

    fix: create uploads directory if not exists

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -3,6 +3,7 @@ import { FileService } from './file.service'
 import { FileController } from './file.controller'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { path } from 'app-root-path'
+import * as fs from 'fs'
 
 @Module({
 	imports: [
@@ -14,4 +15,12 @@ import { path } from 'app-root-path'
 	controllers: [FileController],
 	providers: [FileService],
 })
-export class FileModule {}
+export class FileModule {
+	constructor() {
+		const uploadsPath = `${path}/uploads`
+		if (!fs.existsSync(uploadsPath)) {
+			fs.mkdirSync(uploadsPath, { recursive: true })
+			console.log(`Created uploads directory: ${uploadsPath}`)
+		}
+	}
+}
